8446d2e1899375076a08dde2653b68e9f6269971
josiftepe <39495707+josiftepe@users.noreply.github.com>
2018-07-21 15:43:58 +0200

    Create main.cpp

---
+++ Modulo Summation/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+typedef long long ll;
+int n;
+int arr[200005];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+   cin >> n;
+    ll ret=0;
+   for(int i=0;i<n;i++){
+    cin>>arr[i];
+    ret += arr[i];
+   }
+   int tmp=0;
+cout<< ret-n<<endl;
+ 
+    return 0;
+}
