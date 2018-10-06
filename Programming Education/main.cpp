3a182a0d3a58f6f29308c1d48f4000a2bcbaaa64
josiftepe <39495707+josiftepe@users.noreply.github.com>
2018-10-06 18:15:20 +0200

    AtCoder

---
+++ Programming Education/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <set>
+#include <map>
+#include <sstream>
+#include <cstdio>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <iomanip>
+#include <fstream>
+//#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int INF = (1 << 30);
+const ll inf = (1LL << 60LL);
+const int maxn = 3e5 + 10;
+ 
+int main(int argc, const char * argv[]) {
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    if(n == 1){
+        return 0 * printf("Hello World");
+    }
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
+ 
